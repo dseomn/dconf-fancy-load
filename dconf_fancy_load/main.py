7059e525a15c29ae38eb446834b14d37d1566db4
David Mandelberg <david@mandelberg.org>
2025-02-17 16:09:12 -0500

    Format with isort and black

--- dconf_fancy_load/main.py
+++ dconf_fancy_load/main.py
@@ -45,37 +45,37 @@ import jsonschema
 import yaml
 
 _SCHEMA = {
-    '$schema': 'http://json-schema.org/draft-04/schema#',
-    'type': 'array',
-    'items': {
-        'oneOf': [
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "array",
+    "items": {
+        "oneOf": [
             {
-                'type': 'object',
-                'required': ['dir'],
-                'properties': {
-                    'dir': {
-                        'type': 'string',
+                "type": "object",
+                "required": ["dir"],
+                "properties": {
+                    "dir": {
+                        "type": "string",
                     },
-                    'reset': {
-                        'type': 'boolean',
+                    "reset": {
+                        "type": "boolean",
                     },
-                    'children': {
-                        '$ref': '#',
+                    "children": {
+                        "$ref": "#",
                     },
                 },
             },
             {
-                'type': 'object',
-                'required': ['key'],
-                'properties': {
-                    'key': {
-                        'type': 'string',
+                "type": "object",
+                "required": ["key"],
+                "properties": {
+                    "key": {
+                        "type": "string",
                     },
-                    'reset': {
-                        'type': 'boolean',
+                    "reset": {
+                        "type": "boolean",
                     },
-                    'value': {
-                        'type': 'string',
+                    "value": {
+                        "type": "string",
                     },
                 },
             },
@@ -85,165 +85,176 @@ _SCHEMA = {
 
 
 def _is_key(config_item, parent):
-  """Returns true if the item is a key, false if it's a directory.
+    """Returns true if the item is a key, false if it's a directory.
 
-  Args:
-    config_item: A single item from the YAML config, as a Python dict.
-    parent: DConf path to the item's parent directory.
+    Args:
+      config_item: A single item from the YAML config, as a Python dict.
+      parent: DConf path to the item's parent directory.
 
-  Raises:
-    ValueError: The item is neither or both.
-  """
-  if 'dir' in config_item and 'key' in config_item:
-    raise ValueError(
-        'Item in {!r} has both \'dir\' and \'key\' attrs.'.format(parent))
-  elif 'dir' not in config_item and 'key' not in config_item:
-    raise ValueError(
-        'Item in {!r} has neither a \'dir\' nor a \'key\' attr.'.format(parent))
-  return 'key' in config_item
+    Raises:
+      ValueError: The item is neither or both.
+    """
+    if "dir" in config_item and "key" in config_item:
+        raise ValueError(
+            "Item in {!r} has both 'dir' and 'key' attrs.".format(parent)
+        )
+    elif "dir" not in config_item and "key" not in config_item:
+        raise ValueError(
+            "Item in {!r} has neither a 'dir' nor a 'key' attr.".format(parent)
+        )
+    return "key" in config_item
 
 
 def _set_keys_in_dir(path, values, subprocess_run, dry_run=False):
-  """Sets DConf keys in a directory.
+    """Sets DConf keys in a directory.
 
-  Args:
-    path: Directory.
-    values: Map from relative key to value to set in the directory.
-    subprocess_run: Normally subprocess.run, but can be overriden in tests.
-    dry_run: If true, just print actions.
-  """
-  keyfile = ['[/]\n']
-  # Sort keys to make unit testing easier. (It doesn't matter to dconf load.)
-  for key, value in sorted(values.items(), key=lambda kv: kv[0]):
-    keyfile.append('{}={}\n'.format(key, value))
-  keyfile = ''.join(keyfile)
-  if dry_run:
-    print('Load: {}\n{}'.format(path, textwrap.indent(keyfile, '  ')))
-  else:
-    subprocess_run(
-        ['dconf', 'load', path],
-        input=keyfile,
-        text=True,
-        check=True)
+    Args:
+      path: Directory.
+      values: Map from relative key to value to set in the directory.
+      subprocess_run: Normally subprocess.run, but can be overriden in tests.
+      dry_run: If true, just print actions.
+    """
+    keyfile = ["[/]\n"]
+    # Sort keys to make unit testing easier. (It doesn't matter to dconf load.)
+    for key, value in sorted(values.items(), key=lambda kv: kv[0]):
+        keyfile.append("{}={}\n".format(key, value))
+    keyfile = "".join(keyfile)
+    if dry_run:
+        print("Load: {}\n{}".format(path, textwrap.indent(keyfile, "  ")))
+    else:
+        subprocess_run(
+            ["dconf", "load", path], input=keyfile, text=True, check=True
+        )
 
 
 def _reset_path(path, subprocess_run, preserve=(), dry_run=False):
-  """Selectively resets a key or directory.
+    """Selectively resets a key or directory.
 
-  Args:
-    path: Absolute path to selectively reset, e.g., '/', '/foo/', or '/foo/bar'.
-    preserve: Collection of absolute child paths to not reset. Dirs end in '/',
-      keys don't. This is ignored if path is a key.
-    subprocess_run: Normally subprocess.run, but can be overriden in tests.
-    dry_run: If true, just print actions.
-  """
-  if not preserve or not path.endswith('/'):
-    if dry_run:
-      print('Reset: {}'.format(path))
-    else:
-      subprocess_run(['dconf', 'reset', '-f', path], check=True)
-    return
-  dconf_list = subprocess_run(
-      ['dconf', 'list', path],
-      stdout=subprocess.PIPE,
-      text=True,
-      check=True)
-  for child in dconf_list.stdout.splitlines():
-    child_path = path + child
-    if child_path in preserve:
-      continue
-    _reset_path(
-        child_path,
-        subprocess_run, {x for x in preserve if x.startswith(child_path)},
-        dry_run=dry_run)
-
-
-def load_config(config, path='/', dry_run=False, subprocess_run=subprocess.run):
-  """Loads DConf values from yaml.
-
-  Args:
-    config: Python list parsed from a YAML config. See module docstring for
-      format.
-    path: Root DConf path of the config, e.g., '/' or '/foo/'.
-    dry_run: If true, just print actions.
-    subprocess_run: Normally subprocess.run, but can be overriden in tests.
-
-  Returns:
-    Collection of paths to not reset, suitable for passing as the preserve
-    argument to _reset_path.
-  """
-  preserve = set()
-  values = {}  # Map from key to value to set in the current directory.
-  for config_item in config:
-    if _is_key(config_item, path):
-      config_item_path = path + config_item['key']
-      if 'value' in config_item:
-        values[config_item['key']] = config_item['value']
-        preserve.add(config_item_path)
-      if 'reset' in config_item:
-        if config_item['reset']:
-          _reset_path(config_item_path, subprocess_run, dry_run=dry_run)
+    Args:
+      path: Absolute path to selectively reset, e.g., '/', '/foo/', or '/foo/bar'.
+      preserve: Collection of absolute child paths to not reset. Dirs end in '/',
+        keys don't. This is ignored if path is a key.
+      subprocess_run: Normally subprocess.run, but can be overriden in tests.
+      dry_run: If true, just print actions.
+    """
+    if not preserve or not path.endswith("/"):
+        if dry_run:
+            print("Reset: {}".format(path))
         else:
-          preserve.add(config_item_path)
-    else:
-      config_item_path = path + config_item['dir'] + '/'
-      if 'children' in config_item:
-        config_item_preserve = load_config(
-            config_item['children'],
-            config_item_path,
+            subprocess_run(["dconf", "reset", "-f", path], check=True)
+        return
+    dconf_list = subprocess_run(
+        ["dconf", "list", path], stdout=subprocess.PIPE, text=True, check=True
+    )
+    for child in dconf_list.stdout.splitlines():
+        child_path = path + child
+        if child_path in preserve:
+            continue
+        _reset_path(
+            child_path,
+            subprocess_run,
+            {x for x in preserve if x.startswith(child_path)},
             dry_run=dry_run,
-            subprocess_run=subprocess_run)
-      else:
-        config_item_preserve = set()
-      if 'reset' in config_item:
-        if config_item['reset']:
-          _reset_path(
-              config_item_path,
-              subprocess_run,
-              config_item_preserve,
-              dry_run=dry_run)
-        # Preserve the directory either way. Either it was already reset, so
-        # preserving it is an optimization, or the entire directory needs to be
-        # preserved.
-        preserve.add(config_item_path)
-      else:
-        # No explicit reset parameter, so preserve the preserved paths from the
-        # children.
-        preserve.update(config_item_preserve)
-  if values:
-    _set_keys_in_dir(path, values, subprocess_run, dry_run=dry_run)
-  return preserve
+        )
+
+
+def load_config(config, path="/", dry_run=False, subprocess_run=subprocess.run):
+    """Loads DConf values from yaml.
+
+    Args:
+      config: Python list parsed from a YAML config. See module docstring for
+        format.
+      path: Root DConf path of the config, e.g., '/' or '/foo/'.
+      dry_run: If true, just print actions.
+      subprocess_run: Normally subprocess.run, but can be overriden in tests.
+
+    Returns:
+      Collection of paths to not reset, suitable for passing as the preserve
+      argument to _reset_path.
+    """
+    preserve = set()
+    values = {}  # Map from key to value to set in the current directory.
+    for config_item in config:
+        if _is_key(config_item, path):
+            config_item_path = path + config_item["key"]
+            if "value" in config_item:
+                values[config_item["key"]] = config_item["value"]
+                preserve.add(config_item_path)
+            if "reset" in config_item:
+                if config_item["reset"]:
+                    _reset_path(
+                        config_item_path, subprocess_run, dry_run=dry_run
+                    )
+                else:
+                    preserve.add(config_item_path)
+        else:
+            config_item_path = path + config_item["dir"] + "/"
+            if "children" in config_item:
+                config_item_preserve = load_config(
+                    config_item["children"],
+                    config_item_path,
+                    dry_run=dry_run,
+                    subprocess_run=subprocess_run,
+                )
+            else:
+                config_item_preserve = set()
+            if "reset" in config_item:
+                if config_item["reset"]:
+                    _reset_path(
+                        config_item_path,
+                        subprocess_run,
+                        config_item_preserve,
+                        dry_run=dry_run,
+                    )
+                # Preserve the directory either way. Either it was already reset, so
+                # preserving it is an optimization, or the entire directory needs to be
+                # preserved.
+                preserve.add(config_item_path)
+            else:
+                # No explicit reset parameter, so preserve the preserved paths from the
+                # children.
+                preserve.update(config_item_preserve)
+    if values:
+        _set_keys_in_dir(path, values, subprocess_run, dry_run=dry_run)
+    return preserve
 
 
 def main(config_directory, dry_run=False, subprocess_run=subprocess.run):
-  """Main.
+    """Main.
 
-  Args:
-    dry_run: If true, nothing is changed in dconf, and actions are printed
-      instead.
-    subprocess_run: Normally subprocess.run, but can be overriden in tests.
-  """
-  jinja_env = jinja2.Environment(
-      loader=jinja2.FileSystemLoader(config_directory), autoescape=False)
-  with os.scandir(config_directory) as dir_iter:
-    for entry in sorted(dir_iter, key=lambda entry: entry.name):
-      if not entry.name.endswith('.yaml'):
-        continue
-      config = yaml.safe_load(
-          jinja_env.get_template(entry.name).render(env=os.environ))
-      jsonschema.validate(config, _SCHEMA)
-      load_config(config, dry_run=dry_run, subprocess_run=subprocess_run)
+    Args:
+      dry_run: If true, nothing is changed in dconf, and actions are printed
+        instead.
+      subprocess_run: Normally subprocess.run, but can be overriden in tests.
+    """
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(config_directory), autoescape=False
+    )
+    with os.scandir(config_directory) as dir_iter:
+        for entry in sorted(dir_iter, key=lambda entry: entry.name):
+            if not entry.name.endswith(".yaml"):
+                continue
+            config = yaml.safe_load(
+                jinja_env.get_template(entry.name).render(env=os.environ)
+            )
+            jsonschema.validate(config, _SCHEMA)
+            load_config(config, dry_run=dry_run, subprocess_run=subprocess_run)
 
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--dry-run', dest='dry_run', action='store_const', const=True)
-  parser.add_argument(
-      '--force', dest='dry_run', action='store_const', const=False)
-  args = parser.parse_args()
-  if args.dry_run is None:
-    raise RuntimeError('Specify either --dry-run or --force.')
-  main(
-      os.path.join(os.getenv('HOME'), '.config', 'dconf-fancy-load', 'conf.d'),
-      dry_run=args.dry_run)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dry-run", dest="dry_run", action="store_const", const=True
+    )
+    parser.add_argument(
+        "--force", dest="dry_run", action="store_const", const=False
+    )
+    args = parser.parse_args()
+    if args.dry_run is None:
+        raise RuntimeError("Specify either --dry-run or --force.")
+    main(
+        os.path.join(
+            os.getenv("HOME"), ".config", "dconf-fancy-load", "conf.d"
+        ),
+        dry_run=args.dry_run,
+    )
